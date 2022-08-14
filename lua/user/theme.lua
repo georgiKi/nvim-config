deea15a83d1b717b4460fbb6f6b77013d5e23fd3
Georgi Kirilov <georgikirilov97@gmail.com>
2022-08-14 18:23:30 +0200

    update theme config

--- lua/user/theme.lua
+++ lua/user/theme.lua
@@ -7,19 +7,7 @@ end
 -- Theme config
 theme.setup({
   theme_style = "dark_default",
+  transparent = true,
   function_style = "italic",
   sidebars = {"qf", "vista_kind", "terminal", "packer"},
-
-  -- Change the "hint" color to the "orange" color, and make the "error" color bright red
-  colors = {hint = "orange", error = "#ff0000"},
-
-  -- Overwrite the highlight groups
-  overrides = function(c)
-    return {
-      htmlTag = {fg = c.red, bg = "#282c34", sp = c.hint, style = "underline"},
-      DiagnosticHint = {link = "LspDiagnosticsDefaultHint"},
-      -- this will remove the highlight groups
-      TSField = {},
-    }
-  end
 })
