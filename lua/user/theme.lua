ca80d62232abf11fc561846208479df8c0a5160e
Georgi Kirilov <georgikirilov97@gmail.com>
2022-08-14 17:26:47 +0200

    add theme and plugins config

---
+++ lua/user/theme.lua
@@ -0,0 +1,25 @@
+-- Use a protected call so we don't error out on first use
+local status_ok, theme = pcall(require, "github-theme")
+if not status_ok then
+  return
+end
+
+-- Theme config
+theme.setup({
+  theme_style = "dark_default",
+  function_style = "italic",
+  sidebars = {"qf", "vista_kind", "terminal", "packer"},
+
+  -- Change the "hint" color to the "orange" color, and make the "error" color bright red
+  colors = {hint = "orange", error = "#ff0000"},
+
+  -- Overwrite the highlight groups
+  overrides = function(c)
+    return {
+      htmlTag = {fg = c.red, bg = "#282c34", sp = c.hint, style = "underline"},
+      DiagnosticHint = {link = "LspDiagnosticsDefaultHint"},
+      -- this will remove the highlight groups
+      TSField = {},
+    }
+  end
+})
