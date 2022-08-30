7276f46390c048ad73a87fcbabfdeeb0fe61cf66
Georgi Kirilov <georgikirilov97@gmail.com>
2022-08-30 09:28:05 +0200

    update null ls config with format on save

--- lua/lsp/nullLs.lua
+++ lua/lsp/nullLs.lua
@@ -1,7 +1,30 @@
-require("null-ls").setup({
+local status_ok, nullLs = pcall(require, "null-ls")
+if not status_ok then
+  return
+end
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+nullLs.setup({
     sources = {
-        require("null-ls").builtins.formatting.stylua,
-        require("null-ls").builtins.diagnostics.eslint,
-        require("null-ls").builtins.completion.spell,
+        nullLs.builtins.formatting.prettier,
+        nullLs.builtins.completion.spell,
+        nullLs.builtins.completion.luasnip,
+        nullLs.builtins.completion.tags,
+        nullLs.builtins.diagnostics.eslint,
+        nullLs.builtins.formatting.prettier_d_slim
     },
+    -- Format on save
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    vim.lsp.buf.formatting_seq_sync()
+                end
+            })
+        end
+    end
 })
--- lua/lsp/tsserver.lua
+++ lua/lsp/tsserver.lua
@@ -1,1 +1,6 @@
-require'lspconfig'.tsserver.setup {}
+local status_ok, lspConfig = pcall(require, "lspconfig")
+if not status_ok then
+  return
+end
+
+lspConfig.tsserver.setup {}
