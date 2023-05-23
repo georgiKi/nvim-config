80e289acccf0171fd955c861e6c8251983f5712d
Georgi Kirilov <georgikirilov97@gmail.com>
2023-05-23 16:27:26 +0200

    fix nvim tree config override

--- lua/plugins/configs/essential/nvim_tree.lua
+++ lua/plugins/configs/essential/nvim_tree.lua
@@ -5,33 +5,29 @@ return {
     repo_path = "kyazdani42/nvim-tree.lua",
     enabled = true,
     dependencies = "kyazdani42/nvim-web-devicons",
-    config = {
-        view = {
-           adaptive_size = false,
-           side = "left",
-           width = 40,
-       },
-       renderer = {
-           group_empty = true,
-       },
-       hijack_directories = {
-           auto_open = false
-       }
-    },
-    artifacts = function(plugin)
-        utils.keymap("n", "<leader>e", ":NvimTreeToggle<cr>", { desc = "File Explorer" })
-        utils.keymap("n", "<leader>E", ":NvimTreeFindFile<cr>", { desc = "Find File In Explorer" })
-
+    configFunc = function()
         local circles = require('circles')
-
         circles.setup({ icons = { empty = '', filled = '', lsp_prefix = ''}})
 
-        plugin.setup({
+        return {
+            view = {
+                adaptive_size = true,
+                side = "left",
+                width = 40,
+            },
             renderer = {
-                icons = {
+                group_empty = true,
+                 icons = {
                     glyphs = circles.get_nvimtree_glyphs(),
                 },
             },
-        })
+            hijack_directories = {
+                auto_open = false
+            }
+        }
+    end,
+    artifacts = function()
+        utils.keymap("n", "<leader>e", ":NvimTreeToggle<cr>", { desc = "File Explorer" })
+        utils.keymap("n", "<leader>E", ":NvimTreeFindFile<cr>", { desc = "Find File In Explorer" })
     end
 }
