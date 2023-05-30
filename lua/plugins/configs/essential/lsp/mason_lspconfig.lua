0cba7be06adac4faf6c14023de1a85fd65b77a3b
Georgi Kirilov <georgikirilov97@gmail.com>
2023-05-30 17:14:54 +0200

    disable notify messages on hover

--- lua/plugins/configs/essential/lsp/mason_lspconfig.lua
+++ lua/plugins/configs/essential/lsp/mason_lspconfig.lua
@@ -19,8 +19,8 @@ return {
     artifacts = function(plugin)
         local capabilities = require("cmp_nvim_lsp").default_capabilities()
         local handlers =  {
-            ["textDocument/hover"] =  vim.lsp.with(vim.lsp.handlers.hover, {border = "single"}),
-            ["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.signature_help, {border = "signle"}),
+            ["textDocument/hover"] =  vim.lsp.with(vim.lsp.handlers.hover, {border = "single", silent = true}),
+            ["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.signature_help, {border = "single", silent = true}),
         }
 
         plugin.setup_handlers {
