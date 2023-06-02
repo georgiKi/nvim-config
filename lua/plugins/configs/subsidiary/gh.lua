f535f0591e86ffd8bdf32eeaa7ac8d5b858a6de1
Georgi Kirilov <georgikirilov97@gmail.com>
2023-06-02 16:55:57 +0200

    add more key maps for gh plugin

--- lua/plugins/configs/subsidiary/gh.lua
+++ lua/plugins/configs/subsidiary/gh.lua
@@ -9,7 +9,10 @@ return {
        require('litee.gh').setup({ icon_set = "nerd" })
 
        utils.keymap("n", "<leader>Go", ":GHOpenPR <CR>", { desc = "Open PR" })
-       utils.keymap("n", "<leader>Gc", ":GHCreateThread <CR>", { desc = "Create PR Thread" })
        utils.keymap("n", "<leader>Gx", ":GHClosePR <CR>", { desc = "Close PR" })
+       utils.keymap("n", "<leader>Gr", ":GHRefreshPR <CR>", { desc = "Refresh PR" })
+       utils.keymap("n", "<leader>Gc", ":GHCreateThread <CR>", { desc = "Create PR Thread" })
+       utils.keymap("n", "<leader>Gs", ":GHStartReview <CR>", { desc = "Start Review" })
+       utils.keymap("n", "<leader>Gb", ":GHSubmitReview <CR>", { desc = "Submit Review" })
     end
 }
