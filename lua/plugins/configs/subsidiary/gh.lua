f2deca4e0dce68d11b4f476a60f298bb30fbae3c
Georgi Kirilov <georgikirilov97@gmail.com>
2023-06-02 15:54:23 +0200

    add key maps for gh plugin

--- lua/plugins/configs/subsidiary/gh.lua
+++ lua/plugins/configs/subsidiary/gh.lua
@@ -1,3 +1,5 @@
+local utils = require "core.utils"
+
 return {
     repo_path = "ldelossa/gh.nvim",
     enabled = true,
@@ -5,5 +7,9 @@ return {
     artifacts = function ()
        require('litee.lib').setup()
        require('litee.gh').setup({ icon_set = "nerd" })
+
+       utils.keymap("n", "<leader>Go", ":GHOpenPR <CR>", { desc = "Open PR" })
+       utils.keymap("n", "<leader>Gc", ":GHCreateThread <CR>", { desc = "Create PR Thread" })
+       utils.keymap("n", "<leader>Gx", ":GHClosePR <CR>", { desc = "Close PR" })
     end
 }
