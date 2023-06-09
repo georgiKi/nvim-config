6c9fb25ad6e9a4edaaa772202a7903665d064cff
Georgi Kirilov <georgikirilov97@gmail.com>
2023-06-09 22:31:07 +0200

    update commit

--- lua/core/config.lua
+++ lua/core/config.lua
@@ -55,7 +55,7 @@ vim.diagnostic.config({
     }
 })
 
--- Show line diagnostics automatically in hover window
+-- Diagnostics automatically on hover
 vim.o.updatetime = 250
 vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
 
