9d51a6897abe736a0a024d4e49fa63633909326c
Georgi Kirilov <georgikirilov97@gmail.com>
2023-06-14 12:46:48 +0200

    lazy load auto session plugin

--- lua/plugins/configs/essential/auto_session.lua
+++ lua/plugins/configs/essential/auto_session.lua
@@ -1,10 +1,17 @@
+local utils = require "core.utils"
+
 vim.o.sessionoptions="buffers"
 
 return {
     name = "auto-session",
     repo_path = "rmagatti/auto-session",
     enabled = true,
+    lazy = true,
+    event = "VeryLazy",
     config = {
         log_level = "error",
-    }
+    },
+    artifacts = function ()
+        utils.keymap("n", "<leader>s", "<cmd>:SessionRestore<cr>", { desc = "Restore Previous Session" })
+    end
 }
