1103f86bcdc7f381ffea0eaa4dd434bd6240bc32
Georgi Kirilov <georgikirilov97@gmail.com>
2023-07-03 11:35:36 +0200

    theme config minor change

--- lua/plugins/configs/ui/theme.lua
+++ lua/plugins/configs/ui/theme.lua
@@ -19,7 +19,7 @@ return {
         specialReturn = true,       -- special highlight for the return keyword
         specialException = true,    -- special highlight for exception handling keywords
         transparent = true,        -- do not set background color
-        dimInactive = true,        -- dim inactive window `:h hl-NormalNC`
+        dimInactive = false,        -- dim inactive window `:h hl-NormalNC`
         globalStatus = true,       -- adjust window separators highlight for laststatus=3
         terminalColors = true,      -- define vim.g.terminal_color_{0,17}
         theme = "default",          -- Load "default" theme or the experimental "light" theme
@@ -47,7 +47,6 @@ return {
         overrides = function ()
             return {
                 WinSeparator = { fg = "#1F1F28" },
-                NormalNC = { bg = "#000000" }
             }
         end
     },
