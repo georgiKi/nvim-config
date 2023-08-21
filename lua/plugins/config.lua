08e3a11a025a2a9770ebf1d9521f9a9f75aff961
Georgi Kirilov <georgikirilov97@gmail.com>
2023-08-21 10:14:37 +0200

    add treesitter context plugin

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -40,6 +40,7 @@ do return {
     require (subsidiary .. "neoformat"),
     require (subsidiary .. "nvim-colorizer"),
     require (subsidiary .. "nvim_code_action_menu"),
+    require (subsidiary .. "nvim_treesitter_context"),
     require (subsidiary .. "toggleterm"),
     require (subsidiary .. "reach"),
     require (subsidiary .. "which_key"),
---
+++ lua/plugins/configs/subsidiary/nvim_treesitter_context.lua
@@ -0,0 +1,8 @@
+return {
+    name = "treesitter-context",
+    repo_path = "nvim-treesitter/nvim-treesitter-context",
+    enabled = true,
+    lazy = true,
+    event = "VeryLazy",
+    config = {}
+}
