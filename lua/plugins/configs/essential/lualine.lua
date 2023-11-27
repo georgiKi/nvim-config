0047698d23cdaee86542c5ce829ad4e1228c2d9b
Georgi Kirilov <georgi.kirilov97@gmail.com>
2023-11-27 10:57:48 +0100

    update nvim lualine config

--- lua/plugins/configs/essential/lualine.lua
+++ lua/plugins/configs/essential/lualine.lua
@@ -8,7 +8,7 @@ return {
         options = {
             icons_enabled = true,
             component_separators = '|',
-            section_separators = { left = '', right = '' },
+            section_separators = { left = '', right = '' },
             disabled_filetypes = { 'NvimTree' },
             refresh = {
                 statusline = 1000,
@@ -17,26 +17,22 @@ return {
             }
         },
         sections = {
-            lualine_a = {
-              { 'mode', separator = { left = '' }, right_padding = 2 },
-            },
-            lualine_b = { 'filename', 'branch' },
-            lualine_c = {},
-            lualine_x = {},
-            lualine_y = { 'filetype' },
-            lualine_z = {
-              { 'location', separator = { right = '' }, left_padding = 2 },
-            },
-          },
-          inactive_sections = {
-            lualine_a = { 'filename' },
-            lualine_b = {},
-            lualine_c = {},
-            lualine_x = {},
+            lualine_a = {'mode'},
+            lualine_b = {'diff', 'diagnostics'},
+            lualine_c = {'filename', 'branch'},
+            lualine_x = {'filetype'},
             lualine_y = {},
-            lualine_z = { 'location' },
-          },
-  },
+            lualine_z = {'location'}
+        },
+        inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = {'filename'},
+            lualine_x = {'location'},
+            lualine_y = {},
+            lualine_z = {}
+        }
+    },
     artifacts = function(lualine)
         local custom_theme = require'lualine.themes.everforest'
 
@@ -49,8 +45,8 @@ return {
 
         lualine.setup {
            options = {
-                theme = custom_theme,
+                theme = custom_theme
            }
-       }
+        }
     end
 }
