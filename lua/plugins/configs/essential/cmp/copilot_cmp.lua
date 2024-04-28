d185ab7696d8d1c9dafae552ef0c2f934d3143a7
Georgi Kirilov <georgi.kirilov97@gmail.com>
2024-04-28 11:01:33 +0200

    cleanup

--- lua/plugins/configs/essential/cmp/copilot_cmp.lua
+++
@@ -1,9 +0,0 @@
-return {
-    name = "copilot_cmp",
-    repo_path = "zbirenbaum/copilot-cmp",
-    priority = 950,
-    enabled = false,
-    lazy = true,
-    event = "VeryLazy",
-    config = {}
-}
