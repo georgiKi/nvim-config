c89fe84f316446b259573dbd9342f91b651cac76
Georgi Kirilov <georgi.kirilov97@gmail.com>
2024-07-14 11:49:54 +0200

    update plugins

--- lua/plugins/configs/subsidiary/which_key.lua
+++ lua/plugins/configs/subsidiary/which_key.lua
@@ -9,6 +9,7 @@ return {
             breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
             separator = "", -- symbol used between a key and it's label
             group = "+", -- symbol prepended to a group
+            rules = false
         },
 
         popup_mappings = {
